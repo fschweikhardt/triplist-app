3297c2346880dd6e52fd9be8517c8505c60ea941
fschweikhardt <schweikhardt.web@gmail.com>
2021-01-11 15:33:17 -0500

    setup login

--- src/Login.js
+++ src/Login.js
@@ -1,37 +1,85 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './App.css'
+//import config from './config'
 
-export default function Login(props) {
+export default class Login extends React.Component {
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        const userLogin = {
+            username: e.target.username.value,
+            password: e.target.password.value
+        }
+
+        // const usernameLogin = 'tony'
+        // const passwordLogin = 'asdf'
+
+        //const { username, password } = e.target.value
+
+        console.log(userLogin.username)
+
+        // const options = {
+        //     method: 'GET',
+        //     headers: {
+        //         'content-type': 'application/json',
+        //         //'Authorization': `Bearer ${usernameLogin}:${passwordLogin}` 
+        //     },
+        //     body: JSON.stringify(newUser)
+        // }
+
+        // fetch(`${config.API_ENDPOINT}/users`, options)
+        //     .then(res => {
+        //         if (!res.ok) {
+        //             return res.json().then(e => Promise.reject(e))
+        //         }
+        //         return res.json()
+        //     })
+        //     .catch(error => {
+        //         console.error({ error })
+        //     }) 
+
+        // e.target.reset()
+    }
+
+    render() {
 
     return (
         <div>
+            <form onSubmit={this.handleSubmit}>
             <h2>
                 Log in
             </h2>
             <label>
                 username
-                <input>
+                <input
+                    type='text'
+                    id='username'
+                    name='username'>
                 </input>
             </label>
             <br />
             <label>
                 password
-                <input>
+                <input
+                    type='text'
+                    id='password'
+                    name='password'>
                 </input>
             </label>
             <br />
             <button
                 type='submit'>
-                <Link to='/home'>
-                Log In
-                </Link>
+                SUBMIT
             </button>
             <br />    
             <button
-                onClick={()=>props.history.push('/')}>
+               // onClick={()=>props.history.push('/')}
+            >
                 GO BACK
-            </button> 
+            </button>
+            </form> 
         </div>
     )
+}
 }
