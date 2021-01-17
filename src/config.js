59486d7cafbbe552b5ae638609cf8e2cc1a15f65
fschweikhardt <schweikhardt.web@gmail.com>
2021-01-17 13:53:19 -0500

    deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 //eslint-disable-next-line
 export default { 
     API_ENDPOINT: process.env.REACT_APP_API_BASE_URL,
-    API_TOKEN: process.env.API_TOKEN
+    API_TOKEN: process.env.API_TOKEN || 123456789
 }
