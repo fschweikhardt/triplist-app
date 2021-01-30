dc9207d0be982ac3da0f547de176d2d91f6b5290
fschweikhardt <schweikhardt.web@gmail.com>
2021-01-29 21:56:33 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-
 //eslint-disable-next-line
 export default { 
-    API_ENDPOINT: process.env.REACT_APP_API_BASE_URL,
+    API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || //'https://triplist-api.herokuapp.com',
+    'http://localhost:8000',
     API_TOKEN: process.env.API_TOKEN || 123456789,
     JWT_SECRET: process.env.JWT_SECRET,
     IMG_KEY: process.env.IMG_KEY ||'563492ad6f91700001000001cda85fe2c69f49e99526ea5ef4fd1ace'
