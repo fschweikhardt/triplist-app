d73466cd96554479ca8fa453dd9fd7e1d075ebe5
fschweikhardt <schweikhardt.web@gmail.com>
2021-01-31 13:36:37 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@
 export default { 
     API_ENDPOINT: process.env.REACT_APP_API_BASE_URL //|| 'https://triplist-api.herokuapp.com',
     || 'http://localhost:8000',
-    JWT_SECRET: process.env.JWT_SECRET,
     API_TOKEN: process.env.API_TOKEN,
     IMG_KEY: process.env.IMG_KEY || '563492ad6f91700001000001cda85fe2c69f49e99526ea5ef4fd1ace',
 }
