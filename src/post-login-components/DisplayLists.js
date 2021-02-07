ff748931a1dd313314189df7723f605f6b7965a9
fschweikhardt <schweikhardt.web@gmail.com>
2021-02-07 13:10:32 -0500

    Update DisplayLists.js

--- src/post-login-components/DisplayLists.js
+++ src/post-login-components/DisplayLists.js
@@ -10,7 +10,7 @@ export default class DisplayLists extends React.Component {
 
         return (
             <div>
-               <ul style={{backgroundColor: 'lightGrey'}} className='group-list'>
+               <ul style={{backgroundColor: 'lightGrey', padding: '20px'}} className='group-list'>
                    {this.context.lists.map( list => {
                        return (
                            <li 
